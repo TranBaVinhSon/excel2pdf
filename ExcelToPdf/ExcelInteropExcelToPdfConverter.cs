46d5bda2ac1bd8cb24819532c6ce927fe7edd762
TranBaVinhSon <tranbavinhson@gmail.com>
2019-11-22 20:01:25 +0900

    init

---
+++ ExcelToPdf/ExcelInteropExcelToPdfConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ExcelToPdf
+{
+    public class ExcelInteropExcelToPdfConverter
+    {
+        public void ConvertToPdf(IEnumerable<string> excelFilesPathToConvert)
+        {
+            using (var excelApplication = new ExcelApplicationWrapper())
+            {
+                foreach (var excelFilePath in excelFilesPathToConvert)
+                {
+                    var thisFileWorkbook = excelApplication.ExcelApplication.Workbooks.Open(excelFilePath);
+                    string newPdfFilePath = Path.Combine(
+                        Path.GetDirectoryName(excelFilePath),
+                        $"{Path.GetFileNameWithoutExtension(excelFilePath)}.pdf");
+
+                    thisFileWorkbook.ExportAsFixedFormat(
+                        Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF,
+                        newPdfFilePath);
+
+                    thisFileWorkbook.Close(false, excelFilePath);
+                    Marshal.ReleaseComObject(thisFileWorkbook);
+                }
+            }
+        }
+    }
+}
---
+++ ExcelToPdf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ExcelToPdf
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string directoryWithExcelFiles;
+            if (args.Length == 0)
+            {
+                // If no directory path is passed as argument, consider the current process directory
+                directoryWithExcelFiles = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+                directoryWithExcelFiles = Path.GetFullPath(args[0]);
+            }
+
+            var excelFilesToConvert = Directory.EnumerateFiles(directoryWithExcelFiles, "*.xls");
+            var excelInteropExcelToPdfConverter = new ExcelInteropExcelToPdfConverter();
+
+            try
+            {
+                excelInteropExcelToPdfConverter.ConvertToPdf(excelFilesToConvert);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Something went wrong: {ex.Message}");
+                Environment.ExitCode = -1;
+                return;
+            }
+
+            Console.WriteLine("Operation completed");
+        }
+    }
+}
